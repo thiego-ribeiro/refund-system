e2911e3103ac2e9d3d8d1e172be873db736a9f96
Thiego Ribeiro <thiego.hl@gmail.com>
2025-02-19 13:37:46 -0300

    criando função que adiciona um novo item na lista

--- scripts.js
+++ scripts.js
@@ -44,9 +44,62 @@ form.onsubmit = (event) => {
     amount: amount.value,
     created_at: new Date(),
   }
+  // Chama a função que irá adicionar o item na lista.
+  expenseAdd(newExpense)
 }
 
+// Adiciona um novo Item na lista.
+function expenseAdd(newExpense) {
+  try {    
+    // Cria o elemento para adicionar o item (li) na lista (ul).
+    const expenseItem = document.createElement("li")
+    expenseItem.classList.add("expense") 
 
+    // Cria o ícone da categoria.
+    const expenseIcon = document.createElement("img")
+    expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`)
+    expenseIcon.setAttribute("alt", newExpense.category_name)
+
+    // Cria a info da despesa.
+    const expenseInfo = document.createElement("div")
+    expenseInfo.classList.add("expense-info")
+
+    // Cria o nome da despesa.
+    const expenseName = document.createElement("strong")
+    expenseName.textContent = newExpense.expense
+
+    // Cria a categoria da despesa.
+    const expenseCategory = document.createElement("span")
+    expenseCategory.textContent = newExpense.category_name
+
+    // Adiciona nome e categoria na div das informações da despesa.
+    expenseInfo.append(expenseName, expenseCategory)
+
+    // Adiciona o valor da despesa.
+    const expenseAmount = document.createElement("span")
+    expenseAmount.classList.add("expense-amount")
+    expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount.toUpperCase().replace("R$", "")}`
+
+    // Cria o icone de remover.
+    const removeIcon = document.createElement("img")
+    removeIcon.classList.add("remove-icon")
+    removeIcon.setAttribute("src", "./img/remove.svg")
+    removeIcon.setAttribute("alt", "remover")
+  
+    // Adiciona as informações no item.
+    expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon)
+
+    // Adiciona o item na lista.
+    expenseList.append(expenseItem)
+
+    // Atualiza os totais.
+    updateTotals()
+
+  } catch (error) {
+    alert("Não foi possível atualizar a lista de despesas.")      
+    console.log(error)
+  }
+}
 
 
 
